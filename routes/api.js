3c738661ab46c7fc2348e60506953464ee5cf903
KarimAmador <karimalejandroAC@gmail.com>
2024-08-14 12:58:38 -0600

    feat: add api/translate route handler

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,9 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
+      if (!Object.hasOwn(req.body, 'locale') || !Object.hasOwn(req.body, 'text')) return res.json({ error: 'Required field(s) missing' });
+      if (!req.body.text) return res.json({ error: 'No text to translate' });
       
+      res.json(translator.translate(req.body.locale, req.body.text));
     });
 };
